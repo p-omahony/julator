31a2b9d5e959f56122a02aba7fb14402264fe84d
phmny <paul.omahony@outlook.fr>
2022-01-11 16:49:15 +0100

    add LSTM model

---
+++ models/lstm.py
@@ -0,0 +1,27 @@
+import numpy as np
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, Dropout
+
+def lstm_data_prepare(dataX, dataY, seq_len, n_patterns, vocab_size):
+    X = np.reshape(dataX, (n_patterns, seq_len, 1))
+    X = X / float(vocab_size)
+    y = np_utils.to_categorical(dataY)
+
+    return X, y
+
+class LSTM:
+    """docstring for ."""
+
+    def __init__(self, X, y):
+        self.X = X
+        self.y = y
+
+    def build_model(self):
+        model = Sequential()
+        model.add(LSTM(256, input_shape=(self.X.shape[1], self.X.shape[2]), return_sequences=True))
+        model.add(Dropout(0.2))
+        model.add(LSTM(256))
+        model.add(Dropout(0.2))
+        model.add(Dense(self.y.shape[1], activation='softmax'))
+        return model
